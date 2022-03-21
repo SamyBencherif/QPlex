c034e0c1d0f602302236e45f1f1bbc2ceb236d55
Samy Bencherif <samybencherif@live.com>
2022-03-21 07:55:39 -0700

    .

--- python/runTests.py
+++ python/runTests.py
@@ -1,7 +1,9 @@
 from subprocess import check_output as call
 from subprocess import check_call as ccall
-from subprocess import STDOUT
+from subprocess import STDOUT, DEVNULL
 import os
+
+caller = "python3"
 
 _bailed = False
 
@@ -18,45 +20,39 @@ def abrT(r):
       a = line[line.find(", ")+2:] + "\n"
       a = a.replace(", in <module>", "")
       o += "  " + a
-    elif ":" in line:
+    elif ": " in line:
       o += "  " + line.split(": ")[0] + ":  " + line.split(": ")[1] + "\n"
     else:
       o += line + "\n"
   return o[:-1]
 
-def test(prog=None, expected=""):
+def test(prog, expected=""):
   print(f" === {prog or 'MAIN MODULE'} === ")
   if _bailed:
     return
+  os.system(f"cat tests/{prog}")
+  print(f" === ======================= === ")
   if prog and not os.path.exists('tests/'+prog):
     expected = "FILE EXISTS"
     result = "IT DOES NOT"
   else:
+    result = str(call(caller+" qplex.py tests/"+prog+"; echo -n", shell=1, stderr=STDOUT), 'utf-8')
+    resultc = 0
     try:
-      # i think windows is > NUL or something
-      if prog:
-        result = str(call("python3 qplex.py tests/"+prog+"; echo -n", shell=1, stderr=STDOUT), 'utf-8')
-        resultc = 0
-        try:
-          ccall("python3 qplex.py tests/"+prog, shell=1, stderr=DEVNULL)
-        except:
-          resultc = 1
-      else:
-        result = str(call("python3 qplex.py; echo -n", shell=1, stderr=STDOUT), 'utf-8')
-      result = f'{result}'
-      expected = f'{expected}'
-    except Exception as ex:
-      result = ex
+      ccall(caller+" qplex.py tests/"+prog, shell=1, stderr=DEVNULL)
+    except:
+      resultc = 1
   if expected==result:
     print("Passed!")
+    print("")
     return True
   elif not resultc:
-    print(f"EXPECTED: \"{expected}\"\nRESULT: \"{result}\"\n")
+    print(f"EXPECTED: \"{expected}\"\nRESULT: \"{result}\"")
   else:
-    print(abrT(result))
+    print(abrT(result).strip() or "ERROR")
 
-# test the module, then test the features
-test("helloworld.qpl", "Hello, World!") or bail()
+# test the features
+test("helloworld.qpl", "Hello, World!\n") or bail()
 test("assignmentAndLookup.qpl") or bail()
 test("functiondefAndCall.qpl") or bail()
 test("functiondef.qpl") or bail()
